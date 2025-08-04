8dc0cbca0212750d9bdbf9cfaf08515b5244accc
devhardik21 <hardikshandilya021@gmail.com>
2025-08-04 13:16:57 +0530

    first commit - hardik

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,29 @@ import { useState } from 'react'
 
 import './App.css'
 
+import { Card } from './components/Card'
+
+import BannerCard from './components/BannerCard'
+import { BannerPage } from './pages/BannerPage'
+import { CategoryPage } from './pages/CategoryPage'
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+      {/* <div className='flex flex-wrap justify-start items-start bg-zinc-100 min-h-screen p-8'>
+        
+        <Card Name="Web Development" url="https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" NumberofCompanies="4" NumberofSub="5"></Card>
+        <Card Name="Web Development" url="https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" NumberofCompanies="4" NumberofSub="5"></Card>
+        <Card Name="Web Development" url="https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" NumberofCompanies="4" NumberofSub="5"></Card>
+     
+
+        <BannerCard url="https://plus.unsplash.com/premium_photo-1678565879444-f87c8bd9f241?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ></BannerCard>
+      </div> */}
+
+    <div>
+       {/* <CategoryPage></CategoryPage> */}
+       <BannerPage></BannerPage>
+    </div>
     </>
   )
 }
---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { URL } from '../constants/api';
+import { Card } from '../components/Card';
+const CategoryPage = () => {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [categories, setCategories] = useState([]);
+    const [DisplayError, SetDisplayError] = useState("");
+    useEffect(() => {
+
+        const FetchCategoryData = async () => {
+            try {
+
+                const response = await axios(`${URL}/api_app/get-category`);
+                setCategories(response.data);
+                setLoading(false) ;
+
+                console.log(response.data);
+                
+
+            } catch (err) {
+                setError(true);
+                setLoading(false);
+                SetDisplayError(err);
+                console.log(err);
+            }
+
+        }
+        FetchCategoryData();
+
+    }, [])
+
+    if (loading) {
+        return (
+            <h2> Your Categories are loading</h2>
+        )
+    }
+
+    if (error) {
+        return (
+            <p className='text-red-600'>We got error while loading the category ie {DisplayError}</p>
+        )
+    }
+    return (
+        <div className='bg-zinc-100'>
+            <div className='grid gap-2 grid-cols-3'>
+                {
+                    categories.listings.map((category, idx) => {
+                        return (
+                            <Card  key={idx} Name={category.CategoryName} url={category.CategoryUrl} NumberofCompanies="4" NumberofSub="5" ></Card>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+    )
+}
+
+export {CategoryPage}
